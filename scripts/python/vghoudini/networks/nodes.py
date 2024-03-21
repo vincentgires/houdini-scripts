8f1f9a0f91de888509a0a1119f945c72cf2ef899
Vincent Girès <vincent@vincent.bio>
2024-03-21 11:39:48 +0100

    Parse all node versions

--- scripts/python/vghoudini/networks/nodes.py
+++ scripts/python/vghoudini/networks/nodes.py
@@ -20,7 +20,8 @@ def get_categories(*categories: CategoryName) -> list[hou.NodeTypeCategory]:
 
 def get_nodes_from_type(
         types: str | list[str],
-        categories: tuple[CategoryName] | str = ()) -> list[hou.Node]:
+        categories: tuple[CategoryName] | str = (),
+        all_versions=True) -> list[hou.Node]:
     nodes = []
     if isinstance(categories, str):
         categories = [categories]
@@ -28,8 +29,15 @@ def get_nodes_from_type(
         if isinstance(types, str):
             types = [types]
         for type_name in types:
-            node_type = category.nodeType(type_name)
-            if node_type is None:
-                continue
-            nodes.extend(list(node_type.instances()))
+            if not all_versions:
+                node_type = category.nodeType(type_name)
+                if node_type is None:
+                    continue
+                nodes.extend(list(node_type.instances()))
+            else:
+                for type_name_, node_type in category.nodeTypes().items():
+                    raw_type_name = hou.hda.componentsFromFullNodeTypeName(
+                        type_name_)[2]
+                    if type_name == raw_type_name:
+                        nodes.extend(list(node_type.instances()))
     return nodes
